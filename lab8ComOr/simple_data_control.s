12678ddb9c14ef976bf16d918022c4e2f5917045
nedondev <nedondev@gmail.com>
2019-04-05 12:25:01 +0700

    ADD: Delete in control data

--- lab8ComOr/simple_data_control.s
+++ lab8ComOr/simple_data_control.s
@@ -40,6 +40,10 @@ _start:
 	push {r12}
 	bleq _search_data
 	pop {r12}
+	cmp r12,#100 @d
+	push {r12}
+	bleq _delete_data
+	pop {r12}
 	cmp r12,#101 @e
 	beq exit
 	pop {r4, r12, lr}
@@ -227,7 +231,179 @@ show_result:
 	mov r7, #4
 	svc 0
 	
+	pop {r4, pc}
+
+_delete_data:
+	push {r4, lr}
+
+	/* Open (Create) File */
+	ldr r0, =newfile
+	mov r1, #0x42 @ create R/W
+	mov r2, #384 @ = 600 (octal)
+	mov r7, #5 @ open (create)
+	svc 0
+
+	cmp r0, #-1 @ open error?
+	beq err
+
+	mov r4, r0 @ save file descriptor
+	
+	/* Show Input String */
+	mov r0, #1 @ stdout – 1 = monitor
+	ldr r1, =input_advice3 @ input string
+	mov r2, #(ip3_end-input_advice3) @ len
+	mov r7, #4
+	svc 0
+
+	/* input read */
+	mov r0,	#1 @ monitor
+
+	ldr r1, =keyword
+	mov r2, #10
+	mov r7, #3
+	svc 0
+
+	mov r3,#0
+	bl fine_line
+
+	cmp r0,#-1
+	push {r0}
+	blne show_del_result
+	pop {r0}
+	cmp r0,#-1
+	push {r0}
+	blne delete_process
+	pop {r0}
+	cmp r0,#-1
+	bleq not_found_del
+
 	pop {r4, pc}	
+
+delete_process:
+	push {r4, lr}
+		
+	mov r5, r0 @delete place
+	
+	/* Open (Create) File */
+	ldr r0, =tempfile
+	mov r1, #0x42 @ create R/W
+	mov r2, #384 @ = 600 (octal)
+	mov r7, #5 @ open (create)
+	svc 0
+	
+	cmp r0, #-1 @ open error?
+	beq err
+ 
+	mov r3, r0 @ save file descriptor
+		
+	mov r12, #0
+	read_and_write:
+		cmp r12, r5
+		addeq r12, r12, #90
+
+		/* lseek */
+		mov r0, r4 @ file descriptor
+		mov r1, r12 @ position
+		mov r2, #0 @ seek_set
+		mov r7, #19
+		svc 0
+
+		/* simple read file*/
+		mov r0,	r4 @ file descriptor
+		ldr r1, =buffer
+		mov r2, #90
+		mov r7, #3
+		svc 0
+
+		mov r8,#90
+		ldrb r0, [r1]
+		cmp r0, #0
+		moveq r8,#1
+		/* simple write file */
+		mov r0, r3
+		ldr r1, =buffer @ input string	
+		mov r2, r8
+		mov r7, #4
+		svc 0
+
+		add r12, r12, #90
+		ldrb r0, [r1]
+		cmp r0, #0
+		bne read_and_write
+
+	/* Close file */
+	mov r0, r3
+	mov r7, #6
+	svc 0
+
+	/* Close file */
+	mov r0, r4
+	mov r7, #6
+	svc 0
+		
+	/*unlink file*/
+	ldr r0, =newfile
+	mov r7, #0xa @ unlink (delete)
+	svc 0
+
+	/*rename temp to original*/
+	ldr r0, =tempfile
+	ldr r1, =newfile
+	mov r7, #0x26 @ unlink (delete)
+	svc 0
+
+	pop {r4, pc}
+
+show_del_result:
+	push {r4, lr}
+
+	add r3,r0,#0
+	/* Show Delete  */
+	mov r0, #1 @ stdout – 1 = monitor
+	ldr r1, =del @ input string
+	mov r2, #(del_end-del)
+	mov r7, #4
+	svc 0
+
+	/* lseek */
+	mov r0, r4 @ file descriptor
+	mov r1, r3 @ position
+	mov r2, #0 @ seek_set
+	mov r7, #19
+	svc 0
+
+	/* simple read */
+	mov r0,	r4 @ file descriptor
+	ldr r1, =buffer
+	mov r2, #90
+	mov r7, #3
+	svc 0
+
+	/* Show Delete line */
+	mov r0, #1 @ stdout – 1 = monitor
+	ldr r1, =buffer @ input string
+	mov r2, #90
+	mov r7, #4
+	svc 0
+	
+	pop {r4, pc}	
+
+not_found_del:
+	push {r4, lr}
+
+	/* Close file */
+	mov r0, r4
+	mov r7, #6
+	svc 0
+
+	/* Show NOTFOUND!! */
+	mov r0, #1 @ stdout – 1 = monitor
+	ldr r1, =search_err@ input string
+	mov r2, #(sher_end-search_err)
+	mov r7, #4
+	svc 0
+	
+	pop {r4, pc}
 
 not_found:
 	push {r4, lr}
@@ -257,11 +433,16 @@ err:    mov r4, r0
 errmsg: .asciz "create failed" 
 errmsgend: 
 newfile: .asciz "/home/pi/Documents/Computer_Organize_And_Assembly/lab8ComOr/testFile/specificData2.txt" 
+tempfile: .asciz "/home/pi/Documents/Computer_Organize_And_Assembly/lab8ComOr/testFile/tempspecificData.txt"
+del: .asciz "Delete : "
+del_end:
+input_advice3: .asciz "Input a ID for delete: "
+ip3_end:
 input_advice2: .asciz "Input a ID for search: "
 ip2_end:
 input_advice1: .asciz "Input a string in form like (ID) (Content)\n Example: 62010000 John Smith\n: " 
 ip1_end: 
-input_advice0: .asciz "Select function (Case Sensitive) \n\ti (insert Data)\n\ts (search Data)\n\te (exit)\n : "
+input_advice0: .asciz "Select function (Case Sensitive) \n\ti (insert Data)\n\ts (search Data)\n\td (delete Data)\n\te (exit)\n : "
 ip0_end: 
 search_err: .asciz "NOTFOUND!!\n"
 sher_end:
